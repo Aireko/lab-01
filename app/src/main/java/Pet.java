813d4243593d41c5fe69afdff622a38a118af8e5
Phil <jly2360@outlook.com>
2026-01-09 11:26:38 -0700

    constructors for Pet.java class
    step 4 of lab

--- app/src/main/java/Pet.java
+++ app/src/main/java/Pet.java
@@ -4,4 +4,13 @@ public class Pet {
     private String name;
     private Date birthDate;
 
+    public Pet(String name) {
+        this.name = name;
+        this.birthDate = new Date(); // this will set birthdate to current date
+    }
+
+    public Pet(String name, Date birthDate) {
+        this.name = name;
+        this.birhtDate = birthDate;
+    }
 }
